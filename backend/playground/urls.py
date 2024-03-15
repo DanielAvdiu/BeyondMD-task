7df68c60fc587768edcfc20acb2502237a3a1752
Daniel-0507 <davdiu20@epoka.edu.al>
2024-03-15 19:36:07 -0400

    adding screenshots
    adding screenshots of the project

--- backend/playground/urls.py
+++ backend/playground/urls.py
@@ -3,7 +3,6 @@ from . import views
 from .views import DownloadPDF
 
 urlpatterns = [
-    path('hello/', views.say_hello),
     path('definition/', views.find_free),
     path('download/', DownloadPDF, name='donwload_pdf'),
     path('exchange/', views.exchange, name='exchange_currencies'),
